33f819ef703d804da9ce935bf37ec9006d260996
Adam Chainz <adam@adamj.eu>
2015-02-13 14:56:26 +0000

    Add SimpleSetField for forms

--- django_mysql/fields/set_fields.py
+++ django_mysql/fields/set_fields.py
@@ -1,11 +1,14 @@
 # -*- coding:utf-8 -*-
+from __future__ import absolute_import
+
 from django.core import checks
 from django.db.models import (CharField, IntegerField, SubfieldBase,
                               Transform)
 from django.db.models.lookups import Contains
 from django.utils import six
 
-from django_mysql.validators import SetMaxLengthValidator
+from ..forms import SimpleSetField
+from ..validators import SetMaxLengthValidator
 
 __all__ = ['SetCharField']
 
@@ -21,7 +24,7 @@ class SetCharField(six.with_metaclass(SubfieldBase, CharField)):
         super(SetCharField, self).__init__(**kwargs)
 
         if self.size:
-            self.validators.append(SetMaxLengthValidator(self.size))
+            self.validators.append(SetMaxLengthValidator(int(self.size)))
 
     def check(self, **kwargs):
         errors = super(SetCharField, self).check(**kwargs)
@@ -124,6 +127,15 @@ class SetCharField(six.with_metaclass(SubfieldBase, CharField)):
     def value_to_string(self, obj):
         vals = self._get_val_from_obj(obj)
         return self.get_prep_value(vals)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': SimpleSetField,
+            'base_field': self.base_field.formfield(),
+            'max_length': self.size,
+        }
+        defaults.update(kwargs)
+        return super(SetCharField, self).formfield(**defaults)
 
 
 @SetCharField.register_lookup
